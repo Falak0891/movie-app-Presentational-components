9c5025e707142bbfc5c3200b3bcce569fad15877
falak Chandni <falak.chandni@codingninjas.com>
2023-01-31 12:44:21 +0530

    State in React

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 class MovieCard extends React.Component{
+    constructor(){
+        super();
+        //Creating the state object 
+        this.state = {
+                title: 'The Avengers', 
+                plot:
+                    "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.", 
+                poster:
+                    'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+                rating: '8.0',
+                price: 199 
+        } 
+  }
 
     render(){
+        //Destructing the state object in render function
+        const {title, plot, poster, price, rating} =  this.state;
           
         return(
             <div className="main">
@@ -12,20 +27,20 @@ class MovieCard extends React.Component{
 
                     {/**Left section of Movie Card */}
                     <div className="left">
-                        <img alt="poster" src='https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg' />
+                        <img alt="poster" src={poster} />
                     </div>
                     
                     {/**Right section Movie Card */}
                     <div className="right">
 
                         {/**Title, plot, price of the movie */}
-                        <div className="title">The Avengers</div>
-                        <div className="plot">Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.</div>
-                        <div className="price">Rs. 199</div>
+                        <div className="title">{title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">Rs. {price}</div>
 
                         {/**Footer starts here with ratings, stars and buttons */}
                         <div className="footer">
-                            <div className="rating">8.5</div>
+                            <div className="rating">{rating}</div>
 
                             {/**Star image with increase and decrease buttons and star count */}
                             <div className="star-dis">
